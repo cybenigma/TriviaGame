bb723a899c5da1159ee7397e6cabcf0b741f29c8
michael curtis <michael curtis>
2017-03-19 23:01:34 -0400

    version 1 save

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var interval;
+var question {
+
+}
+var questions ["What is the name of the second 'Toejam and Earl' game?", "What was the third 'Toejam and Earl' game originally going to be called?",
+			   "What is the name of Toejam and Earl's short, green friend in the second 'Toejam and Earl' game?","Who is the female alien Toejam is attracted to in the second 'Toejam and Earl' game?",
+			   "Which of these Earthlings is not in 'Toejam and Earl'?", "The two modes in 'Toejam and Earl' are Random World and Perfect World.",
+			   "What is the real name of the Anti-Funk in 'Mission to Earth'?", "What do you use to capture Earthlings in the second game?",
+			   "What do you have to collect for Lamont the Funkapotumus in 'Mission to Earth'?", "The Wahinis (Hula Dancers) do damage to you."];
+
